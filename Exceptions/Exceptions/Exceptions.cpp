e553055feaf64b69d037b3453f2b74d17304de2f
ctlauritzen12 <90056755+ctlauritzen12@users.noreply.github.com>
2024-08-14 15:38:40 -0700

    Add files via upload

---
+++ Exceptions/Exceptions/Exceptions.cpp
@@ -0,0 +1,119 @@
+// Exceptions.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <exception>
+
+class CustomException: public std::exception
+{
+public: const char* what() const noexcept override {
+        return "Custom Exception\n";
+}
+} customE;
+
+bool do_even_more_custom_application_logic()
+{
+    // TODO: Throw any standard exception
+
+    throw std::logic_error("Error in logic");
+
+
+    std::cout << "Running Even More Custom Application Logic." << std::endl;
+
+    return true;
+}
+void do_custom_application_logic()
+{
+    // TODO: Wrap the call to do_even_more_custom_application_logic()
+    //  with an exception handler that catches std::exception, displays
+    //  a message and the exception.what(), then continues processing
+    std::cout << "Running Custom Application Logic." << std::endl;
+
+    try {
+
+        if (do_even_more_custom_application_logic())
+        {
+            std::cout << "Even More Custom Application Logic Succeeded." << std::endl;
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Custom Application Error\n" << e.what() <<std::endl;
+    }
+    // TODO: Throw a custom exception derived from std::exception
+    //  and catch it explictly in main
+
+    throw customE;
+
+    std::cout << "Leaving Custom Application Logic." << std::endl;
+
+}
+
+float divide(float num, float den)
+{
+    // TODO: Throw an exception to deal with divide by zero errors using
+    //  a standard C++ defined exception 
+   // try
+    //{
+
+        if (den == 0)
+        {
+            throw std::runtime_error("Divide by zero is undefined");
+        }
+    
+    return (num / den);
+}
+
+void do_division() noexcept
+{
+    //  TODO: create an exception handler to capture ONLY the exception thrown
+    //  by divide.
+    
+    float numerator = 10.0f;
+    float denominator = 0;
+
+    try
+    {
+        auto result = divide(numerator, denominator);
+        std::cout << "divide(" << numerator << ", " << denominator << ") = " << result << std::endl;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Divide error\n" << e.what() << std::endl;
+    }
+
+}
+
+int main()
+{
+    std::cout << "Exceptions Tests!" << std::endl;
+
+    // TODO: Create exception handlers that catch (in this order):
+    //  your custom exception
+    //  std::exception
+    //  uncaught exception 
+    //  that wraps the whole main function, and displays a message to the console.
+    //do_division();
+   // do_custom_application_logic();
+    try
+    {
+        do_division();
+        do_custom_application_logic();
+    }
+        
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception written to main:  " << e.what() << std::endl;
+    }
+    catch (...)
+    {
+        std::cerr << "Whoops" << std::endl;
+    }
+
+    std::cout << "Exceptions Tests completed!" << std::endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
